d9c0427ab549d2bd4e19d8b40f8b8e1e2cc03743
lorenzodegiorgi <62562522+lorenzodegiorgi@users.noreply.github.com>
2023-05-23 15:57:24 +0200

    Update k8_operator.py

--- k8_operator.py
+++ k8_operator.py
@@ -1,5 +1,14 @@
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
 
+with DAG(
+    dag_id='k8s_dag',
+    schedule_interval='@daily',
+    start_date=datetime(2022, 3, 1),
+    catchup=False
+) as dag:
     write_xcom = KubernetesPodOperator(
         namespace="default",
         image="alpine",
@@ -11,10 +20,8 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
         task_id="write-xcom",
         get_logs=True,
     )
-
     pod_task_xcom_result = BashOperator(
         bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
         task_id="pod_task_xcom_result",
     )
-
     write_xcom >> pod_task_xcom_result

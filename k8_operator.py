deaa413764b4aab610abf067967b60e8e8dc3d02
lorenzodegiorgi <62562522+lorenzodegiorgi@users.noreply.github.com>
2023-05-23 15:56:15 +0200

    Update k8_operator.py

--- k8_operator.py
+++ k8_operator.py
@@ -1,13 +1,20 @@
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 
-k = KubernetesPodOperator(
-    name="hello-dry-run",
-    image="debian",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
-    labels={"foo": "bar"},
-    task_id="dry_run_demo",
-    do_xcom_push=True,
-)
+    write_xcom = KubernetesPodOperator(
+        namespace="default",
+        image="alpine",
+        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
+        name="write-xcom",
+        do_xcom_push=True,
+        is_delete_operator_pod=True,
+        in_cluster=True,
+        task_id="write-xcom",
+        get_logs=True,
+    )
 
-k.dry_run()
+    pod_task_xcom_result = BashOperator(
+        bash_command="echo \"{{ task_instance.xcom_pull('write-xcom')[0] }}\"",
+        task_id="pod_task_xcom_result",
+    )
+
+    write_xcom >> pod_task_xcom_result

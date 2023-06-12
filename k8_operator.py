7c2680d8c7286bc37710b83e8f3e67551861dc95
lorenzodegiorgi <62562522+lorenzodegiorgi@users.noreply.github.com>
2023-06-12 17:02:17 +0200

    Update k8_operator.py

--- k8_operator.py
+++ k8_operator.py
@@ -14,7 +14,7 @@ with DAG(
     write_xcom = KubernetesPodOperator(
         namespace="airflow-cluster",
         image="alpine",
-        cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
+        cmds=["sh", "-c", "echo "Hello Word"; mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
         name="write-xcom",
         do_xcom_push=True,
         is_delete_operator_pod=False,
